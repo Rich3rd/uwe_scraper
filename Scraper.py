bfbdb699ed2ae6b7497c3b6aba94cb57e95b1eb5
Richard <richard_xf95@hotmail.com>
2017-03-18 00:44:04 +0000

    quiekbkbquitkbkbkbkbkbkbkbkbkbkbkbkbkbqquitkbkbqquitkbkbkbkbqquqwequitkbkbquitqidssdfqhelpkbkbkbkbkbkbkbq!:q:q!
    Uskbuses dryscrape:kb:wq:wq commitmskbessage:wq cokbommitmes
    
    kukukukukukukukukukukuklkrkrikrkrikrkuklklkrkrkrkukrkukbkbkbkbkbkb:q:q!

--- Scraper.py
+++ Scraper.py
@@ -3,6 +3,7 @@ import urllib.request
 import base64
 import string
 import requests
+import dryscrape
 
 class BlackboardSession():
     def __init__(self, user, password):
@@ -26,9 +27,25 @@ payload = {
             }
 
 
-session = requests.post('https://blackboard.uwe.ac.uk/webapps/login/',data=payload)
+#session = requests.post('https://blackboard.uwe.ac.uk/webapps/login/',data=payload)
+
+session = dryscrape.Session();
+session.visit('https://blackboard.uwe.ac.uk/webapps/login/')
+name = session.at_xpath('//*[@name="user_id"]') # Where <input name="username">
+name.set('n2-terjing')
+password = session.at_xpath('//*[@name="password"]') # Where <input name="password">
+password.set('vellapushFare19')
+# Push the button
+name.form().submit()
+session.visit('https://blackboard.uwe.ac.uk/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1')
 #print(session.text)
-print(session.cookies)
-response = requests.get('https://blackboard.uwe.ac.uk/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_19_1',cookies=session.cookies)
-soup = BeautifulSoup(response.text,'html.parser')
-print(soup.text)
+#print(session.cookies)
+#headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+#response = requests.get('https://blackboard.uwe.ac.uk/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_19_1',cookies=session.cookies,headers=headers)
+#response = requests.get('https://blackboard.uwe.ac.uk/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1',cookies=session.cookies)
+session.wait_for(lambda: session.at_xpath('//*[@id="_186_1termCourses_noterm"]/ul'))
+
+
+soup = BeautifulSoup(session.body(),'html.parser')
+print(soup)
